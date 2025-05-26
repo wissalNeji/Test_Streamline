fb1298afa4120a88de6056ceebc4c06f6d62dac5
wissalNeji <44780166+wissalNeji@users.noreply.github.com>
2025-05-26 21:30:15 +0100

    Update app-2.py

--- app-2.py
+++ app-2.py
@@ -6,5 +6,7 @@ st.write("This app will clean your data and generate a summary.")
 uploaded_file = st.file_uploader("📤 Upload your CSV file", type=["csv"])
 if uploaded_file:
   df = pd.read_csv(uploaded_file)
+  df.fillna(df.mean(numeric_only=True), inplace=True) 
   st.subheader("👀 Preview of Uploaded Data")
   st.dataframe(df.head())
+  st.dataframe(df.info())
