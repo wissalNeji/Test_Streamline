f80d5c7856ab45f4cb3cf7a9ee8e770dbb52392b
wissalNeji <44780166+wissalNeji@users.noreply.github.com>
2025-05-26 21:26:34 +0100

    Update app-2.py

--- app-2.py
+++ app-2.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("AI-Powered Data Cleaner & Summarizer")
 st.write("This app will clean your data and generate a summary.")
+uploaded_file = st.file_uploader("📤 Upload your CSV file", type=["csv"])
+if uploaded_file:
+  df = pd.read_csv(uploaded_file)
+  st.subheader("👀 Preview of Uploaded Data")
+  st.dataframe(df.head())
